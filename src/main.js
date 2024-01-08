37d4ac7ff5d9b2331b94506e37fc5fb0db951147
NguyenXuanBac110 <cactovb@gmail.com>
2024-01-08 20:23:29 +0700

    add

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.js";
 
-import classes from './main.module.css';
+import classes from './output.css';
 
 document.querySelector('#app').innerHTML = `
   <div class="${classes.red}">
