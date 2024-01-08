68ff4d30e0d540c5095ecc818b3a1d77c7b185c9
NguyenXuanBac110 <cactovb@gmail.com>
2024-01-08 17:39:35 +0700

    add

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ["./index.html", "./components/*.{html,js}", "./pages/*.{html,js}"],
-    prefix: "tw-",
+    
     theme: {
         extend: {},
     },
